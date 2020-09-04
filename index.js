28da59e25f74719365a70bc2d2a153998586b2dc
NguyenTrungThang123456 <thangnt.nde18074@vtc.edu.vn>
2020-09-04 18:24:47 +0700

    Added Docker to app

--- index.js
+++ index.js
@@ -9,27 +9,27 @@ app.use(express.urlencoded({ extended: false }));
 
 // Connect to MongoDB
 mongoose
-  .connect(
-    'mongodb://mongo:27017/docker-node-mongo',
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+	.connect('mongodb://mongo:27017/docker', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('MongoDB Connected'))
+	.catch((err) => console.log(err));
 
 const Item = require('./models/Item');
 
 app.get('/', (req, res) => {
-  Item.find()
-    .then(items => res.render('index', { items }))
-    .catch(err => res.status(404).json({ msg: 'No items found' }));
+	Item.find()
+		.then((items) => res.render('index', { items }))
+		.catch((err) => res.status(404).json({ msg: 'No items found' }));
 });
 
 app.post('/item/add', (req, res) => {
-  const newItem = new Item({
-    name: req.body.name
-  });
+	const newItem = new Item({
+		name: req.body.name,
+	});
 
-  newItem.save().then(item => res.redirect('/'));
+	newItem.save().then((item) => res.redirect('/'));
 });
 
 const port = 3000;
